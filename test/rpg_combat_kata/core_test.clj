33fe5512ff36e32ddd7bbef388ee4f293a6ca4a1
Esko Luontola <esko@luontola.fi>
2019-09-04 23:31:11 +0300

    Leiningen project scaffolding

---
+++ test/rpg_combat_kata/core_test.clj
@@ -0,0 +1,7 @@
+(ns rpg-combat-kata.core-test
+  (:require [clojure.test :refer :all]
+            [rpg-combat-kata.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
