ef35ca8470b27ee6789d66aa4f10bdd635d84c70
Esko Luontola <esko@luontola.fi>
2019-09-04 23:40:23 +0300

    Project branding

---
+++ README.md
@@ -0,0 +1,3 @@
+[RPG Combat Kata](https://github.com/ardalis/kata-catalog/blob/master/katas/RPG%20Combat.md)
+implementation in [Clojure](https://clojure.org)
+at [Codebreeze](https://codebreeze.netlify.com)
--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject rpg-combat-kata "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "RPG Combat Kata implementation in Clojure"
+  :url "https://github.com/luontola/codebreeze2019-rpg-combat-kata"
+  :license {:name "WTFPL"
+            :url "http://www.wtfpl.net"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :main ^:skip-aot rpg-combat-kata.core
   :target-path "target/%s"
