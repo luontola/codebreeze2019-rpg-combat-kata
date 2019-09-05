9cd7415aa7b4a10e24ecce6ff03842741df2c1b3
Esko Luontola <esko@luontola.fi>
2019-09-05 15:02:45 +0300

    cannot self-inflict damage

--- src/rpg_combat_kata/character.clj
+++ src/rpg_combat_kata/character.clj
@@ -1,9 +1,12 @@
-(ns rpg-combat-kata.character)
+(ns rpg-combat-kata.character
+  (:import (java.util UUID)))
 
 (def max-health 1000)
 
-(def new {:character/health max-health
-          :character/level 1})
+(defn new []
+  {:character/id (UUID/randomUUID)
+   :character/health max-health
+   :character/level 1})
 
 (defn alive? [character]
   (pos? (:character/health character)))
@@ -11,8 +14,11 @@
 (defn- change-health [health change]
   (max 0 (min max-health (+ health change))))
 
-(defn attack [victim _attacker]
-  (update victim :character/health change-health -1))
+(defn attack [target attacker]
+  (if (= (:character/id target)
+         (:character/id attacker))
+    target
+    (update target :character/health change-health -1)))
 
 (defn heal [character health-points]
   (if (alive? character)
--- test/rpg_combat_kata/character_test.clj
+++ test/rpg_combat_kata/character_test.clj
@@ -4,47 +4,51 @@
 
 (deftest new-character-test
   (testing "starts at max health"
-    (is (= character/max-health (:character/health character/new))))
+    (is (= character/max-health (:character/health (character/new)))))
   (testing "starts at level 1"
-    (is (= 1 (:character/level character/new))))
+    (is (= 1 (:character/level (character/new)))))
   (testing "is alive"
-    (is (character/alive? character/new))))
+    (is (character/alive? (character/new)))))
 
 (deftest attacking-test
   (testing "deals damage to the victim"
-    (let [attacker character/new
-          victim character/new
+    (let [attacker (character/new)
+          victim (character/new)
           damaged-victim (character/attack victim attacker)]
       (is (< (:character/health damaged-victim)
              (:character/health victim)))))
 
+  (testing "cannot self-inflict damage"
+    (let [character (character/new)]
+      (is (= character (character/attack character character)))))
+
   (testing "health becomes 0 if damage is greater than health"
-    (let [attacker character/new
-          victim (assoc character/new :character/health 1)
+    (let [attacker (character/new)
+          victim (assoc (character/new) :character/health 1)
           damaged-victim (-> victim
                              (character/attack attacker)
                              (character/attack attacker))]
       (is (= 0 (:character/health damaged-victim)))))
 
   (testing "dies when health is 0"
-    (let [attacker character/new
-          victim (assoc character/new :character/health 1)
+    (let [attacker (character/new)
+          victim (assoc (character/new) :character/health 1)
           damaged-victim (character/attack victim attacker)]
       (is (not (character/alive? damaged-victim))))))
 
 (deftest healing-test
   (testing "increases health"
-    (let [damaged (assoc character/new :character/health 500)
+    (let [damaged (assoc (character/new) :character/health 500)
           healed (character/heal damaged 100)]
       (is (> (:character/health healed)
              (:character/health damaged)))))
 
   (testing "the dead cannot be healed"
-    (let [damaged (assoc character/new :character/health 0)
+    (let [damaged (assoc (character/new) :character/health 0)
           healed (character/heal damaged 100)]
       (is (= healed damaged))))
 
   (testing "cannot be healed over max health"
-    (let [damaged (assoc character/new :character/health (dec character/max-health))
+    (let [damaged (assoc (character/new) :character/health (dec character/max-health))
           healed (character/heal damaged 100)]
       (is (= character/max-health (:character/health healed))))))
