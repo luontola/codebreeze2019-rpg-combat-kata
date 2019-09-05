50a128e967f618d9a144b94ff190b6cdcc8c9c0d
Esko Luontola <esko@luontola.fi>
2019-09-05 14:46:24 +0300

    refactor

--- src/rpg_combat_kata/character.clj
+++ src/rpg_combat_kata/character.clj
@@ -7,12 +7,12 @@
   (pos? (:character/health character)))
 
 (defn- change-health [health change]
-  (if (zero? health)
-    health
-    (max 0 (+ health change))))
+  (max 0 (+ health change)))
 
 (defn attack [_attacker victim]
   (update victim :character/health change-health -1))
 
 (defn heal [character health-points]
-  (update character :character/health change-health health-points))
+  (if (alive? character)
+    (update character :character/health change-health health-points)
+    character))
