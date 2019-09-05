7b87dbfc1a26c56c29891c9ea405fee84f426f81
Esko Luontola <esko@luontola.fi>
2019-09-05 19:26:08 +0300

    create house

--- test/rpg_combat_kata/prop_test.clj
+++ test/rpg_combat_kata/prop_test.clj
@@ -8,4 +8,12 @@
             :character/health 1
             :character/position {:x 0}}
            (-> (prop/create)
+               (dissoc :character/id)))))
+
+  (testing "customized prop"
+    (is (= {:prop/type :house
+            :character/health 2000
+            :character/position {:x 0}}
+           (-> (prop/create {:prop/type :house
+                             :character/health 2000})
                (dissoc :character/id))))))
